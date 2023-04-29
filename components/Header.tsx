a55e48bd804c48f151bcea0fa846dfb9fb4133a3
Wayne Zhang <waynezhangus@gmail.com>
2023-04-28 17:43:44 -0700

    add prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,25 @@ import { useRouter } from 'next/router';
 import { User } from '../typings';
 
 interface HeaderProps {
-    user?: User;
+  user?: User;
 }
 
-export default function Header({user}: HeaderProps) {
-    const router = useRouter();
-    return (
-        <header className=''>
-
-        </header>
-    )
+export default function Header({ user }: HeaderProps) {
+  const router = useRouter();
+  return (
+    <header className="flex space-x-2 p-4">
+      <span className="mr-auto text-2xl font-bold uppercase">
+        resumaster.ai
+      </span>
+      <a className="link" href="" target="_blank" rel="noopener noreferrer">
+        Link1
+      </a>
+      <a className="link" href="" target="_blank" rel="noopener noreferrer">
+        Link1
+      </a>
+      <a className="link" href="" target="_blank" rel="noopener noreferrer">
+        Link1
+      </a>
+    </header>
+  );
 }
